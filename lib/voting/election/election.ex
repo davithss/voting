6c86a34c9b32f9a811b4cf59263bb5082f3f8d64
Davi Thiesse <davithss@gmail.com>
2022-04-05 11:33:39 -0300

    Create Election schema and migration

---
+++ lib/voting/election/election.ex
@@ -0,0 +1,24 @@
+defmodule Voting.Election do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Voting.Admin
+
+  schema "elections" do
+    field :cover, :string
+    field :end_at, :utc_datetime
+    field :name, :string
+    field :notice, :string
+    field :starts_at, :utc_datetime
+    belongs_to :created_by, Admin
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(election, attrs) do
+    election
+    |> cast(attrs, [:name, :cover, :notice, :starts_at, :end_at])
+    |> validate_required([:name, :cover, :notice, :starts_at, :end_at])
+  end
+end
